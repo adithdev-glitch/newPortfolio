adbec1c872d0e3d5dc8aea87df7609d2e5eb9af6
adithdev-glitch <adith.git46@gmail.com>
2026-05-24 10:37:20 +0530

    deployment

--- src/assests/assest.js
+++ src/assests/assest.js
@@ -1,9 +1,8 @@
 import me from './me.png';
 import cloud from './cloud.png';
-import project_1 from './project-1.png';
+import project_1 from './project-1.webp';
 import project_2 from './project-2.png';
 import project_3 from './chatapp.png';
-import traffic from './trafic.jpeg';
 import trafic2 from './trafic2.png';
 
 export const assets = {me, cloud, project_1, project_2, project_3, traffic, trafic2};
