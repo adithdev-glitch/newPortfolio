0e64cc5d90e879c0ba77371a62d89164700cbc71
adithdev-glitch <adith.git46@gmail.com>
2026-05-24 10:51:45 +0530

    deployment fix

--- src/assests/assest.js
+++ src/assests/assest.js
@@ -1,7 +1,7 @@
 import me from './me.png';
 import cloud from './cloud.png';
 import project_1 from './project-1.webp';
-import project_2 from './project-2.png';
+import project_2 from './project-2.webp';
 import project_3 from './chatapp.png';
 import trafic2 from './trafic2.png';
 
